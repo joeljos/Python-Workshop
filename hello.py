83128cd19bbf4afd528cdac576c530a21c7203ee
Devanamapriyan <joeljose420@gmail.com>
2018-10-11 20:07:50 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,14 +5,7 @@ d = True
 e = False
 h = b"Hello \xf0\x9f\x98\x8e"
 
-print(a+b,c,d,e,h)
+ds1_list = [a,b,c,d,e,h,"hello world"]
+ds2_dict = {"key1":a,"key2":b,"key3":c}
 
-print(a.lower())
-
-def add(num1, num2):
-    result = num1 + num2
-    return result
-
-a = int(input("give your number : "))
-print(type(a),a)
 
